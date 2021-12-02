0ce8d5b9c3763b65cd2eee42a53170491b622636
shutheonetokyo <ss1030ss@icloud.com>
2021-12-02 16:07:07 +0900

    Shopモデルの作成

---
+++ lib/import_csv.rb
@@ -0,0 +1,15 @@
+require "csv"
+
+class ImportCsv
+  # CSVデータのパスを引数として受け取り、インポート処理を実行
+  def self.import(path)
+     CSV.foreach(path, headers: true) do |row|
+       Shop.create!(
+         name: row["name"],
+         address: row["address"],
+         TEL: row["TEL"],
+         URL: row["URL"]
+       )
+     end
+  end
+end
